3fbcfe74accd220b7da1271350cf0e6925242c57
danma-al <danielmazoalarcon@gmail.com>
2025-01-16 19:25:17 -0500

    Descripcion de los cambios unity

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    //Primer codiguito moral =D
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Primer Script");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
