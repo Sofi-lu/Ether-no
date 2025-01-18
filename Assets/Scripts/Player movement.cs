8d2d4585b0a0ad6708595d9240d0d8bbfb431d8d
Reymono514 <Camilo.gallego.514@outlook.com>
2025-01-18 15:29:31 -0500

    New movement script

---
+++ Assets/Scripts/Player movement.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class Playermovement : MonoBehaviour
+{
+        //Creo una variable que le da la velocidad inicial al objeto
+        public float speed = 1.0f;
+        public float rotationSpeed = 64.0f; // Esta variable es para rotar
+        
+        //Variable para saltar
+        public float jump = 5.0f;
+
+        private Rigidbody rb;
+        void Start()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+
+            rb = GetComponent<Rigidbody>();
+        }
+        // Update is called once per frame
+        void Update()
+        {
+            //Movimiento
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+
+            transform.Translate(new Vector3(horizontal, 0.0f, vertical) * Time.deltaTime * speed);
+
+            //Rotacion
+            float rotationY = Input.GetAxis("Mouse X");
+            transform.Rotate(new Vector3(0, rotationY * Time.deltaTime * rotationSpeed, 0));
+
+            //Salto
+            if(Input.GetKeyDown(KeyCode.Space)){
+                rb.AddForce(new Vector3(0, jump, 0), ForceMode.Impulse);
+            }
+        }
+    }
